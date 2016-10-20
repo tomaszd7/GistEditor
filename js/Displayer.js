2a29cf68f6e159f5b2fbc2d178b5025141ee81cc
Tomasz Dąbrowski <tdabro@wp.pl>
2016-10-20 12:52:21 +0200

    Initial commit

---
+++ js/Displayer.js
@@ -0,0 +1,205 @@
+function Displayer(apiObject) {
+
+    this.api = apiObject;
+
+    // html ids for sections 
+    this.menuId = 'menu';
+    this.listId = 'list';
+    this.detailId = 'details';
+    this.userId = 'user';
+    this.btnGetGistsId = 'get-gists';
+    this.loginBtnId = 'login-btn';
+    this.logoutBtnId = 'logout-btn';
+    
+    // gist list classes
+    this.activeClass = 'bg-primary';
+    this.listClass = 'bg-info';
+
+    // header classes
+    this.languageClass = 'language';
+    this.sizeClass = 'size';
+    this.createdClass = 'created';
+    
+    this.currentGistId = null;
+
+    this.displayUser = function (context) {
+        console.log('displaying user');
+        $('#' + context.userId).html(context.api.user).addClass('bg-primary');
+    };
+
+    // callback function 
+    this.displayGistsList = function (context) {
+        // remove current list 
+        $('#' + context.listId + ' div.list-box').remove();
+        // add list of gists 
+        for (var key in context.api.currentGists) {
+            
+            var $div = $('<div>');
+            var $p = $('<p>');
+            $p.attr('id', key);
+            $p.addClass('bg-info text-center');
+            $p.html(context.api.currentGists[key]['description']);
+            // add private/ public as after
+            var $p_public = $('<div>').html(context.api.currentGists[key]['public'] === true ? 'Public' : 'Private');
+            
+            $div.addClass('list-box');
+            $p_public.addClass('list-public bg-primary small');
+            $div.append($p);
+            $div.append($p_public);
+            $('#' + context.listId).append($div);
+        }
+    };
+
+    // callback funciton
+    this.displayGistDetails = function (context, gistId) {
+        // save current gist id for future actions and easy access
+        context.currentGistId = gistId;
+        
+        // change active classes on gist list
+        $('#' + context.listId + ' p.' + context.activeClass)
+                .removeClass(context.activeClass).addClass(context.listClass);
+        $('#' + gistId).removeClass(context.listClass).addClass(context.activeClass);
+        
+        var gistPath = context.api.gists[gistId].files;
+        var filename = Object.keys(gistPath)[0];
+        // add header 
+        
+        $('.' + context.languageClass).text(gistPath[filename].language);
+        $('.' + context.sizeClass).text(gistPath[filename].size);
+        $('.' + context.createdClass).text(context.api.gists[gistId].created_at);
+        
+        // add details 
+
+        var $textarea = $('textarea');
+        //  $details.addClass(context.listClass + ' text-left');
+        var content = gistPath[filename].content;
+        // parse eol into <br> tags - <pre> tag does NOT need this 
+        // content = content.replace(/(?:\r\n|\r|\n)/g, '<br />');
+        $('#' + context.detailId).append($textarea.val(content));
+    };
+
+    
+    this.displayApplication = function(context) {
+        $('#logging').toggleClass('none');
+        $('#application').toggleClass('none');     
+        context.displayUser(context);        
+    };
+    
+    this.displayLoginFailure = function(context) {
+        // adding class and disabling login button
+        $('#login-btn').removeClass('btn-primary').addClass('btn-danger').attr('disabled', 'disabled');        
+    }
+    
+    this.disableSaveButton = function () {
+        $('.header button').attr('disabled', 'disabled');  
+    }
+
+
+    this.gistsObjectCopy = function(gists, bool) {        
+        var newObj = {};                
+        for (var id in gists) {
+            if (bool === 'all') {
+                newObj[id] = gists[id];
+            } else {
+                if (gists[id].public === bool) {                    
+                    newObj[id] = gists[id];
+                }
+            }            
+        }
+        return newObj;
+    };
+
+    this.main = function () {
+
+        var self = this;
+        
+        // listeners 
+
+        // get list of gits for current user button 
+        $('#' + this.btnGetGistsId).click(function (event) {
+            self.api.getUserGists(self, self.displayGistsList); // callback 
+        });
+
+        // get details of gist for current gist 
+        // check if aready not been used - then do not upload again         
+        $('#' + this.listId).click(function (event) {
+            if (event.target.tagName === 'P') {
+                if(self.api.gists[event.target.id].files) {
+                    console.log('already uploaded so redisplay gist');
+                    self.displayGistDetails(self, event.target.id);
+                } else {
+                    self.api.getGistDetails(event.target.id, self, self.displayGistDetails);                    
+                }                
+            }
+            self.disableSaveButton();
+        });
+                
+        // save gist details         
+        $('#' + this.detailId + ' button').click(function(event) {
+            // get current gist content 
+            console.log('inside Save');
+            if (self.currentGistId) {
+                var newContent = $('textarea').val();
+                self.api.editGistDetails(self.currentGistId, newContent);                
+            }       
+            // disabled save button 
+            self.disableSaveButton();          
+        });        
+        
+        // unlock save button when textarea is omdified 
+        $('textarea').keyup(function(event) {
+            $('.header button').removeAttr('disabled');            
+        })
+        
+        // log user 
+        $('#' + this.loginBtnId).click(function(event) {
+            event.preventDefault();
+            var user = $('form input[name="user"]').val();
+            var password = $('form input[name="password"]').val();
+            
+            self.api.getAuthorizations(user, password, self, self.displayApplication, self.displayLoginFailure);
+        });
+        
+        // logout user 
+        $('#' + this.logoutBtnId).click(function(event) {
+            console.log('logging out');
+            location.reload(true);
+        });
+        
+        //unlocking disabled login after login failure
+        $('#logging input').focus(function(event) {
+            var $loginBtn = $('#login-btn');
+            if ( $loginBtn.attr('disabled') === 'disabled') {
+                // unlock 
+                $loginBtn.removeClass('btn-danger').addClass('btn-primary').removeAttr('disabled');
+            }            
+        })
+        
+        
+        // buttons for displaying public/ private gists         
+        $('#buttons').click(function(event) {
+            
+            function reDisplay() {
+                $('#buttons button.btn-primary').removeClass('btn-primary').addClass('btn-info');
+                $(event.target).removeClass('btn-info').addClass('btn-primary');
+                self.displayGistsList(self); 
+            }
+            
+            switch (event.target.name) {                
+                case 'public':
+                    self.api.currentGists = self.gistsObjectCopy(self.api.gists, true);
+                    reDisplay();
+                    break;
+                case 'private':
+                    self.api.currentGists = self.gistsObjectCopy(self.api.gists, false);
+                    reDisplay();
+                    break;
+                case 'all':
+                    self.api.currentGists = self.gistsObjectCopy(self.api.gists, 'all');
+                    reDisplay();
+                    break;                             
+            }
+        });        
+    };
+
+}
---
+++ js/GistApi.js
@@ -0,0 +1,225 @@
+function GistApi() {
+
+    this.host = 'https://api.github.com';
+    this.user = null;
+    this.url = null;
+
+    this.gists = {};
+    this.currentGists = {}; // for display and sorting 
+
+    this.error = null;
+    this.accessNote = 'Access to my gists';
+    this.token = null; // token in 'hashed_token'
+    this.logged = false;
+    this.credentials = null;
+
+    // with callback 
+    this.authenticateUser = function (user, password, context, fnComplete) {     
+        $.ajax({
+            'url': 'https://api.github.com/authorizations',
+            'type': 'POST',
+            'context': this,
+            'headers': {
+                'Authorization': 'Basic ' + btoa(user + ':' + password)
+            },
+            'data': JSON.stringify({
+                'scopes': ['gist'], 
+                'note': 'Access to my gists'
+            }),
+            'success': function(data) {  
+                this.token = data;
+                this.logged = true;
+            },
+            'error': function (xhr, status, error) {
+                this.error = {
+                    'response': xhr.responseJSON,
+                    'status': xhr.status,
+                    'statusText': xhr.statusText
+                };
+                console.log(this.error);
+            },
+            'complete': function () {
+                console.log('New authentication completed');                
+                if (this.logged && fnComplete) {
+                    fnComplete(context);
+                }
+            }
+        });
+    };
+
+    // with callback
+    this.getAuthorizations = function (user, password, context, fnSuccess, fnFailure) {
+        this.user = user;
+        this.url = this.host + '/users/' + this.user + '/gists';
+        this.credentials = 'Basic ' + btoa(user + ':' + password);
+        
+        // clear previous data
+        this.error = null;
+        this.logged = false;
+        this.token = null;        
+        
+        this.user = user;
+    
+        $.ajax({
+            'url': 'https://api.github.com/authorizations',
+            'type': 'GET',
+            'context': this,
+            'headers': {
+                'Authorization': this.credentials
+            },
+            'success': function(data) {
+                // gets array of tokens ... check mine in 'note': this.accessNote
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].note === this.accessNote) {
+                        this.token = data[i];
+                        this.logged = true;
+                        break;
+                    }
+                }                
+                console.log(this.token);                
+            },
+            'error': function (xhr) {
+                this.error = {
+                    'response': xhr.responseJSON,
+                    'status': xhr.status,
+                    'statusText': xhr.statusText
+                };
+                console.log(this.error);
+            },
+            'complete': function () {
+                console.log('Stored authentication completed');
+                // if success - token is already there
+                if (this.logged && fnSuccess) {
+                    fnSuccess(context);
+                } else {            
+                // try to create new token 
+                    if (!this.error && !this.logged) {
+                        this.authenticateUser(user, password, context, fnSuccess);
+                    } else if (this.error && !this.error.status === 401) {     
+                        this.authenticateUser(user, password, context, fnSuccess);
+                    } else {
+                        // login failed so display to user - wrong login 
+                        if (fnFailure) {
+                            fnFailure(context);
+                        }
+                    }
+                }            
+            }            
+        });
+    };                    
+
+
+    this.getUserGists = function (context, fnComplete) {
+        console.log('token ' + this.token.hashed_token);
+        $.ajax({
+            'url': 'https://api.github.com/gists',
+            'headers': {
+                'Accept': 'application/vnd.github.v3+json',
+//                'Authorization': 'token ' + this.token.hashed_token
+                'Authorization': this.credentials
+            },
+            'context': this,
+            'method': 'GET',
+            'success': function (data) {
+                var self = this;
+                if (data) {
+                    data.forEach(function (item) {
+                        self.gists[item.id] = {
+                            'description': item.description,
+                            'created_at': item.created_at,
+                            'url': item.url,
+                            'public': item.public
+                        };
+                        
+                        self.currentGists[item.id] = {
+                            'description': item.description,
+                            'created_at': item.created_at,
+                            'url': item.url,
+                            'public': item.public
+                        };
+                    });
+                    console.log('User gists uploaded');
+                } else {
+                    // no data 
+                }
+            },
+            'error': function (xhr, status) {
+                console.log(xhr, status);
+            },
+            'complete': function () {
+                if (fnComplete) {
+                    fnComplete(context);
+                }
+            }
+        });
+    };
+
+    this.getGistDetails = function (gistId, context, fnComplete) {
+            var gistPath = this.gists[gistId];
+            $.ajax({
+                'url': gistPath.url,
+                'headers': {
+                    'Accept': 'application/vnd.github.v3+json'
+                },
+                'context': this,
+                'success': function (data) {
+                    gistPath['files'] = {};
+                    var filename = Object.keys(data.files)[0];
+                    gistPath['files'][filename] = {
+                        'content': data.files[filename]['content'],
+                        'language': data.files[filename]['language'],
+                        'size': data.files[filename]['size']
+                    };
+                },
+                'error': function (xhr, status) {
+                    console.log(xhr, status);
+                },
+                'complete': function () {
+                    console.log('single gist reloaded');
+                    if (fnComplete) {
+                        fnComplete(context, gistId);
+                    }
+                }
+            });
+    };
+
+    this.editGistDetails = function (gistId, newContent) {
+        
+        var filename = Object.keys(this.gists[gistId].files)[0];
+        
+        // prepare json body 
+        var jsonBody = {};
+        jsonBody['description'] = this.gists[gistId].description;
+        jsonBody['public'] = this.gists[gistId].public;
+        jsonBody['files'] = {};
+        jsonBody['files'][filename] = {
+            'content': newContent
+        };        
+        console.log(jsonBody, JSON.stringify(jsonBody));        
+        // save to api.gists variables
+        console.log(this.gists[gistId].files[filename]);
+        this.gists[gistId].files[filename].content = newContent;
+        
+        $.ajax({
+            'url': this.gists[gistId].url,
+//            'context': this,
+            'headers': {
+                'Accept': 'application/vnd.github.v3+json',
+                'Authorization': this.credentials                
+            },
+            'method': 'PATCH',
+            'data': JSON.stringify(jsonBody),
+            'success': function (data) {
+                console.log('Editted , data');
+            },
+            'error': function (xhr, status) {
+                console.log(xhr, status);
+            },
+            'complete': function () {
+                console.log('edit completed');
+            }
+        });
+
+    };
+
+}
